a1e07b0adafa64388cdebd0ee46ca6052942f447
Povilas Lajus <povilas.lajus@gmail.com>
2023-02-24 15:16:50 +0200

    feat: add passport local mongoose

--- user/src/auth/auth.ts
+++ user/src/auth/auth.ts
@@ -1,51 +1,17 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
+import { Strategy as JWTstrategy } from "passport-jwt";
+import { ExtractJwt } from "passport-jwt";
 import { User } from "../models/user";
 
 passport.use(
-  "signup",
   new LocalStrategy(
     {
       usernameField: "email",
-      passwordField: "password",
     },
-    async (email, password, done) => {
-      try {
-        const user = await User.create({ email, password });
-
-        return done(null, user);
-      } catch (error) {
-        done(error);
-      }
-    }
+    User.authenticate()
   )
 );
 
-passport.use(
-  "login",
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password",
-    },
-    async (email, password, done) => {
-      try {
-        const user = await User.findOne({ email });
-
-        if (!user) {
-          return done(null, false, { message: "User not found" });
-        }
-
-        const validate = await user.isValidPassword(password);
-
-        if (!validate) {
-          return done(null, false, { message: "Wrong Password" });
-        }
-
-        return done(null, user, { message: "Logged in Successfully" });
-      } catch (error) {
-        return done(error);
-      }
-    }
-  )
-);
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
